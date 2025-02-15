b23e69fefdae8ffe5f1eb0a19560d7c5b1322a19
Suren <ananian.suren96@gmail.com>
2025-02-16 03:08:19 +0400

    repelece host

--- infomax/settings.py
+++ infomax/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o1cl1g*q#h8r&7bzppxy^yl8hv2k+u@vf0pk%u^aaqj&uidv9%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.infomax.space', 'infomax.space']
 
 
 # Application definition
